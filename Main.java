9229d0fb615a679c3cadfb198da5c052d764d98a
Katsu <114673266+Katsu8998@users.noreply.github.com>
2022-10-29 11:53:44 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+# Query: 
+# Flags: CaseSensitive
+# ContextLines: 1
+
+package Clock;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 clockblueprint cacl = new clockblueprint();
+		 	}
+}
